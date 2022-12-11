2ef52e97d9ad9236701a64e4dee5df312e476c83
Shufeng Wu <wu666@Shufengs-MacBook-Air.local>
2022-12-11 10:34:01 -0500

    got an error

---
+++ controllers/routes.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require('mongoose');
+const Recipes = require('../models/recipes.js')
+
+//======================//
+//========ROUTES=======//
+//======================//
+
+//========CREATE/POST ROUTE=======ADD CAR
+router.post('/', (req, res) => {
+    Recipes.create(req.body, (err, createdRecipe) =>{
+        res.json(createdRecipe);
+    });
+  });
+
+  //===========Search Route==========
+  router.post('/search', (req, res) => {
+    const searchResult = req.body.search
+    const regex = new RegExp (searchResult,'i')
+
+    Recipes.find({$or: [{name:regex},{mainIngredient:regex},{nationality:regex}]}, (err, showRecipe) =>{
+        res.json(showRecipe);
+    });
+  });
+
+  //========GET/READ ROUTE=======GET CAR
+  router.get('/', (req, res) => {
+    Recipes.find({}, (err, foundRecipe) => {
+        res.json(foundRecipe);
+    });
+  });
+
+
+  //========DELETE ROUTE=======DELETE CAR
+  router.delete('/:id', (req, res)=>{
+    Recipes.findByIdAndRemove(req.params.id, (err, deletedRecipe)=>{
+        res.json(deletedRecipe);
+    });
+  });
+
+
+  //========UPDATE/EDIT ROUTE=======EDIT CAR
+  router.put('/:id', (req, res)=>{
+    Recipes.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedRecipe)=>{
+        res.json(updatedRecipe);
+    });
+  });
+
+  module.exports = router
