394a2c3914405278c563ab1a646d9c1a079a647f
Joe Klein SEI <josephklein87@gmail.com>
2022-12-13 16:12:59 -0500

    fixed bug with ratings

--- models/recipes.js
+++ models/recipes.js
@@ -12,7 +12,7 @@ const recipeSchema = new mongoose.Schema(
     vegetarian: Boolean,
     spicy: Boolean,
     favs: Array,
-    rating: []
+    ratings: {}
 });
 
 const Recipes = mongoose.model('Recipe', recipeSchema);
