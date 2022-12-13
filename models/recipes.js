aadbd7026c8b2b6681bda23adb9ab3c28be6b405
Joe Klein SEI <josephklein87@gmail.com>
2022-12-13 18:47:24 -0500

    changed routes

--- models/recipes.js
+++ models/recipes.js
@@ -12,7 +12,7 @@ const recipeSchema = new mongoose.Schema(
     vegetarian: Boolean,
     spicy: Boolean,
     favs: Array,
-    ratings: {}
+    ratings: [{}]
 });
 
 const Recipes = mongoose.model('Recipe', recipeSchema);
